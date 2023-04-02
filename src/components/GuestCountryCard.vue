01316c32ed3f8a99a922c97a4cbeca5c9683575f
Payel Karmakar <50908900+PayelKarmakar06@users.noreply.github.com>
2023-04-02 23:11:04 +0200

    update sort clause

--- src/components/GuestCountryCard.vue
+++ src/components/GuestCountryCard.vue
@@ -84,8 +84,8 @@ export default {
                 '+' + (elem.value.nr_of_rooms - elem.reference_value.nr_of_rooms) : elem.value.nr_of_rooms - elem.reference_value.nr_of_rooms
             }
           })
-          .sort((a, b) => b.nr_of_rooms - a.nr_of_rooms)
-          .sort((a, b) => b.changeInValue - a.changeInValue)
+          .sort((a, b) => (b.nr_of_rooms - a.nr_of_rooms) && (b.changeInValue - a.changeInValue))
+          // .sort((a, b) => b.changeInValue - a.changeInValue)
     },
     
     computed: {
